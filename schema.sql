fe0ad091b3edbec61f5f016bd40482364ecdf0e7
looh16 <langacustodio@gmail.com>
2022-09-26 14:58:31 +0200

    Add database schema

--- schema.sql
+++ schema.sql
@@ -1,5 +1,16 @@
 /* Database schema to keep the structure of entire database. */
 
+--create database vet_clinic
+CREATE DATABASE vet_clinic;
+
+--create table animals
 CREATE TABLE animals (
-    name varchar(100)
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name varchar(100),
+    date_of_birth date,
+    escape_attempts INT,
+    neutered BOOLEAN NOT NULL,
+    weight_kg decimal,
+    PRIMARY KEY(id)
 );
+
